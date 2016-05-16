80472e56e322a660f40b923e04acf139faa17a03
Cyrus Roshan <CyrusRoshan@users.noreply.github.com>
2016-05-16 16:47:41 -0500

    rename main file, add json status, send error for blank usernames

--- main.go
+++ main.go
@@ -43,11 +43,17 @@ func main() {
 	r.HandleFunc("/{UserNameA}/{UserNameB}/{FromTimeStamp:[0-9]+}", getMessage).
 		Methods("GET")
 
+	// Catch all other requests and send an error
+	r.NotFoundHandler = http.HandlerFunc(errorPage)
+
 	// Listen to port, so we access this at http://localhost:3001
 	http.ListenAndServe(":3001", r)
 }
 
+// for sending messages between two users
 func setMessage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json") //reply with JSON success or failure
+
 	// Read sent data
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -62,6 +68,11 @@ func setMessage(w http.ResponseWriter, r *http.Request) {
 	err = json.Unmarshal(body, &newMessage)
 	if err != nil {
 		panic(err.Error())
+	}
+
+	if newMessage.To == "" || newMessage.From == "" { // actually have to impose this restriction on usernames
+		w.Write([]byte(`{"status": "failure"}` + "\n"))
+		return
 	}
 
 	Messages = append(Messages, newMessage) // Add the new message to the end of the Messages array
@@ -96,22 +107,25 @@ func setMessage(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	w.Write([]byte("Message successfully sent! " + "\n")) // always nice to know if you have a spotty internet connection
+	w.Write([]byte(`{"status": "success"}` + "\n")) // always nice to know if you have a spotty internet connection
 }
 
+// for reading messages between two users after a particular time
 func getMessage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json") // send content in JSON
+
 	vars := mux.Vars(r)
-	UserNameA := vars["UserNameA"]
-	UserNameB := vars["UserNameB"]
-	FromTimeStamp, err := strconv.ParseInt(vars["FromTimeStamp"], 10, 64)
+	userNameA := vars["UserNameA"]
+	userNameB := vars["UserNameB"]
+	fromTimeStamp, err := strconv.ParseInt(vars["FromTimeStamp"], 10, 64)
 	if err != nil {
 		panic(err.Error())
 	}
 
 	// Construct the JSON that we're going to send back. We don't need the array to be sorted.
 	jsonString := "["
-	for _, currMessage := range Users[UserNameA].Convo[UserNameB] {
-		if (*currMessage).At > FromTimeStamp { // If the message is after the the time sent, we can send the message
+	for _, currMessage := range Users[userNameA].Convo[userNameB] {
+		if (*currMessage).At > fromTimeStamp { // If the message is after the the time sent, we can send the message
 			data, err := json.Marshal(*currMessage)
 			if err != nil {
 				panic(err.Error())
@@ -126,7 +140,11 @@ func getMessage(w http.ResponseWriter, r *http.Request) {
 	}
 	jsonString += "]"
 
-	// Change content type so it's actually recognized as JSON, then send it
-	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(jsonString + "\n"))
+}
+
+// for sendiing error messages for every request type not previously defined
+func errorPage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json") // send content in JSON
+	w.Write([]byte(`{"status": "nonexistent page"}` + "\n"))
 }
